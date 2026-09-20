9594c61d5e065aa6444a0b9bfbc2698f4b29b526
jaykisonekerli-dot <jaykisonekerli@gmail.com>
2026-09-20 17:38:41 -0400

    Atualiza compilador Kotlin para 2.4.10

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,10 @@
+buildscript {
+    dependencies {
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.4.10")
+    }
+}
+
 plugins {
     id("com.android.application") version "9.1.1" apply false
-    id("org.jetbrains.kotlin.plugin.compose") version "2.2.10" apply false
+    id("org.jetbrains.kotlin.plugin.compose") version "2.4.10" apply false
 }
